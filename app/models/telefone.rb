9530e3a1756c73e00c1194f52edad54b65b7b7e5
lab-08 <lab-01@ama-desktop.(none)>
2012-11-13 13:26:31 -0200

    Gerenciar telefone

--- app/models/telefone.rb
+++ app/models/telefone.rb
@@ -1,5 +1,5 @@
 class Telefone < ActiveRecord::Base
   belongs_to :aluno
   belongs_to :tipotelefone
-  attr_accessible :ddd, :numero
+  attr_accessible :ddd, :numero, :tipotelefone_id, :aluno_id
 end
