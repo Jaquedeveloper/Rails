a08fa8a1fc3b787d09e84e6c5efaa88c9666b0d3
lab-08 <lab-01@ama-desktop.(none)>
2012-11-13 11:28:55 -0200

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sisprojetop3::Application.config.secret_token = '7706ab66bbe73cb8b1e39d94d9ed77ad2b77331892696542a6490dd290ca23794a25a2a4e501b8ed36f7ae78ccb62cf5d9b0280551a5d16f709c3db628e7d3bb'
